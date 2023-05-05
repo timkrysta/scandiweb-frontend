32adf6cdf687ec349cb223a7ec1651e3d2633b38
donvoldemort <krystatymoteusz@gmail.com>
2023-05-05 11:36:21 +0200

    Updated footre appearance

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,16 @@
 
 function Footer() {
   return (
-    <footer style={{ display: "flex", justifyContent: "center" }}>
-      Scandiweb Test assignment
+    <footer style={{ /* position: "fixed", bottom: "0", */ width: "100%", /* height: "60px", */ backgroundColor: "#fff" }}>
+      {/* "lineHeight":"60px","backgroundColor":"#f5f5f5" */}
+      <div className="container">
+        <hr />
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <strong>
+            Scandiweb Test assignment
+          </strong>
+        </div>
+      </div>
     </footer>
   );
 }
