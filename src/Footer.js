e597b4501fb4b55f5ec2788b49ce055ccf6cfd9c
donvoldemort <krystatymoteusz@gmail.com>
2023-05-05 11:36:34 +0200

    Updated footre appearance

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,7 @@
 
 function Footer() {
   return (
-    <footer style={{ /* position: "fixed", bottom: "0", */ width: "100%", /* height: "60px", */ backgroundColor: "#fff" }}>
-      {/* "lineHeight":"60px","backgroundColor":"#f5f5f5" */}
+    <footer style={{ /* position: "fixed", bottom: "0", */ width: "100%", backgroundColor: "#fff" }}>
       <div className="container">
         <hr />
         <div style={{ display: "flex", justifyContent: "center" }}>
