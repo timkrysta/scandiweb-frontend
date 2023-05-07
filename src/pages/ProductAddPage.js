109b428ae50b645dee92cd973d287cd91e1382b1
donvoldemort <krystatymoteusz@gmail.com>
2023-05-07 11:10:15 +0200

    Improving code readability & formatting

--- src/pages/ProductAddPage.js
+++ src/pages/ProductAddPage.js
@@ -7,60 +7,8 @@ import Footer from '../components/Footer';
 import ProductAddForm from '../components/ProductAddPage/ProductAddForm/ProductAddForm';
 import '../styles/style.scss';
 
-
 function ProductAddPage() {
   const [productType, setProductType] = useState('');
-
-  const createItemList = (items) => {
-    let html = '<ul>';
-    for (let i = 0; i < items.length; i++) {
-      html += `<li>${items[i]}</li>`;
-    }
-    html += '</ul>';
-    return html;
-  };
-
-  const showInputErrors = (input, errors) => {
-    const inputWrapper = input.parentElement;
-    inputWrapper.classList.remove('is-valid');
-    inputWrapper.classList.add('is-invalid');
-    const validationMessage = inputWrapper.parentElement.querySelector('.validation-message');
-    validationMessage.innerHTML = createItemList(errors)
-  };
-
-  const showFormErrors = (formErorrs) => {
-    for (let inputName in formErorrs) {
-      if (formErorrs.hasOwnProperty(inputName)) {
-        const input = document.querySelector(`[name="${inputName}"]`);
-        showInputErrors(input, formErorrs[inputName]);
-      }
-    }
-  };
-
-  const clearValidationMessages = () => {
-    const validationMessages = document.querySelectorAll('.validation-message');
-    validationMessages.forEach((message) => (message.innerHTML = ''));
-  };
-
-  const handleProductFormSubmit = async () => {
-    const form = document.getElementById('product_form');
-
-    try {
-      const response = await fetch(form.action, {
-        method: form.method,
-        body: new FormData(form)
-      });
-
-      if (response.status === 200) {
-        window.location = '/product/list';
-        return;
-      }
-
-      clearValidationMessages();
-      const data = await response.json();
-      showFormErrors(data.error);
-    } catch (error) { }
-  };
 
   const handleProductTypeChange = (event) => {
     setProductType(event.target.value);
@@ -85,6 +33,53 @@ function ProductAddPage() {
       <Footer />
     </>
   );
+}
+
+function createItemList(items) {
+  const listItems = items.map((item) => `<li>${item}</li>`);
+  return `<ul>${listItems.join('')}</ul>`;
+}
+
+function showInputErrors(input, errors) {
+  const inputWrapper = input.parentElement;
+  inputWrapper.classList.remove('is-valid');
+  inputWrapper.classList.add('is-invalid');
+  const validationMessage = inputWrapper.parentElement.querySelector('.validation-message');
+  validationMessage.innerHTML = createItemList(errors);
+}
+
+function showFormErrors(formErrors) {
+  for (const [inputName, errors] of Object.entries(formErrors)) {
+    const input = document.querySelector(`[name="${inputName}"]`);
+    if (input) {
+      showInputErrors(input, errors);
+    }
+  }
+}
+
+function clearValidationMessages() {
+  const validationMessages = document.querySelectorAll('.validation-message');
+  validationMessages.forEach((message) => (message.innerHTML = ''));
+}
+
+async function handleProductFormSubmit(event) {
+  const form = document.getElementById('product_form');
+
+  try {
+    const response = await fetch(form.action, {
+      method: form.method,
+      body: new FormData(form)
+    });
+
+    if (response.status === 200) {
+      window.location = '/product/list';
+      return;
+    }
+
+    clearValidationMessages();
+    const data = await response.json();
+    showFormErrors(data.error);
+  } catch (error) { }
 }
 
 export default ProductAddPage;
