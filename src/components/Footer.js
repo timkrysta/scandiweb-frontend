19d99571c810970ac4397d5002787eefe5153525
donvoldemort <krystatymoteusz@gmail.com>
2023-05-15 20:17:07 +0200

    Added my LinkedIn link to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,9 @@ function Footer() {
         <hr/>
         <div className="centered-container">
           <strong>  
-            Scandiweb Test assignment
+            Scandiweb Test assignment -
           </strong>
+          <a href="https://www.linkedin.com/in/tim-krysta/" target="_blank">Tim Krysta</a>
         </div>
       </div>
     </footer>
